7ff6e43204842cf297c1600c88d850b02394f9b8
twoimo <twoimo@dgu.ac.kr>
2024-09-04 18:17:20 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+from routes import main, auth, inquiry
+from models.user import User
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,19 +11,17 @@ login_manager = LoginManager()
 app = Flask(__name__)
 app.config.from_object(Config)
 
+app.register_blueprint(main.main)
+app.register_blueprint(auth.auth)
+app.register_blueprint(inquiry.inquiry)
+
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
-from models.user import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 with app.app_context():
     db.create_all()
-
-from routes import main, auth, inquiry
-app.register_blueprint(main.main)
-app.register_blueprint(auth.auth)
-app.register_blueprint(inquiry.inquiry)
