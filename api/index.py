30bc7a482e183e2670b3146136dde0da70b45cc5
twoimo <twoimo@dgu.ac.kr>
2024-09-04 18:16:24 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
-from app.models.user import User
+from models.user import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
