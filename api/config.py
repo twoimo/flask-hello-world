44ec74e8c98709786bcac9260ede13bf4a3d691c
twoimo <twoimo@dgu.ac.kr>
2024-09-04 17:54:24 +0900

    Update config.py

--- api/config.py
+++ api/config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'rinkorea!@#$%'  # 비밀 키는 앱의 보안을 위해 필요해요
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://twoimo:~!!1qw23e@twoimo.mysql.pythonanywhere-services.com:3306/twoimo$bbs_db'  # 데이터베이스 연결 정보
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:test1234@localhost/memo_db'  # 데이터베이스 연결 정보
+    SQLALCHEMY_DATABASE_URI = 'postgres://default:J1rmacjwKln3@ep-wild-forest-a19y89pw.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'  # 데이터베이스 연결 정보
     SQLALCHEMY_TRACK_MODIFICATIONS = False
