db4aaa8b8143bf193c1bca44378b09835be483f2
twoimo <twoimo@dgu.ac.kr>
2024-09-04 18:00:54 +0900

    Update

--- api/config.py
+++ api/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'rinkorea!@#$%'  # 비밀 키는 앱의 보안을 위해 필요해요
-    SQLALCHEMY_DATABASE_URI = 'postgres://default:J1rmacjwKln3@ep-wild-forest-a19y89pw.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'  # 데이터베이스 연결 정보
+    SQLALCHEMY_DATABASE_URI = 'postgres://default:J1rmacjwKln3@ep-wild-forest-a19y89pw.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- api/index.py
+++ api/index.py
@@ -14,7 +14,6 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
-
     from app.models.user import User
     @login_manager.user_loader
     def load_user(user_id):
